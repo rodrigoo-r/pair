81a3645e2865f5cd125ead7ce936682c437ef3d8
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-30 11:48:42 -0500

    Refactor pair.h to define generic pair using macro and remove legacy structure

--- pair.h
+++ pair.h
@@ -48,38 +48,10 @@ extern "C"
         return p;                                             \
     }
 
-/**
- * \brief Generic pair structure to hold two pointers.
- *
- * This struct allows storing a pair of pointers to any type of data.
- * It is commonly used to associate two related values without specifying their types.
- *
- * Members:
- *   void *first  - Pointer to the first element of the pair.
- *   void *second - Pointer to the second element of the pair.
- */
-typedef struct
-{
-    void *first;  /**< Pointer to the first element of the pair */
-    void *second; /**< Pointer to the second element of the pair */
-} generic_pair_t;
-
-/**
- * \brief Creates a generic pair from two pointers.
- *
- * This function initializes a \ref pair_t structure with the provided pointers.
- *
- * \param first  Pointer to the first element of the pair.
- * \param second Pointer to the second element of the pair.
- * \return Initialized \ref pair_t containing the two pointers.
- */
-static inline generic_pair_t generic_pair_new(void *first, void *second)
-{
-    generic_pair_t p;
-    p.first = first;   /**< Initialize the first element of the pair */
-    p.second = second; /**< Initialize the second element of the pair */
-    return p;          /**< Return the initialized pair */
-}
+#ifndef FLUENT_LIBC_PAIR_GENERIC_DEFINED
+    DEFINE_PAIR_T(void *, void *, generic_pair)
+#   define FLUENT_LIBC_PAIR_GENERIC_DEFINED 1
+#endif
 
 // ============= FLUENT LIB C++ =============
 #if defined(__cplusplus)
